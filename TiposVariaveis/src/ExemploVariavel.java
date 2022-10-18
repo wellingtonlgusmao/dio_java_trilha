a84d2bfbe6c7f03ea9b9b9d15aab4d6ff1a833a1
wellingtonlgusmao <wellingtonluizgusmao.29@gmail.com>
2022-10-18 11:45:49 -0300

    mod02

---
+++ TiposVariaveis/src/ExemploVariavel.java
@@ -0,0 +1,19 @@
+public class ExemploVariavel {
+
+    public static void main(String[] args) {
+        /*
+         * esta linha é considerada como declaração de variável iniciamos a existência
+         * variavel numero com valor 5 regra: tipo + nome + valor
+         */
+        int numero = 5;
+
+        /*
+         * na linha abaixo iremos alterar o valor do varíavel para 10 observe que o tipo
+         * não é mais necessário, pois a variável já foi declarada anteriormente
+         */
+        numero = 10;
+
+        System.out.print(numero);
+
+    }
+}
