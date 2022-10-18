4d7a89b41a860d13832f8d4623fa9b86e52562cc
wellingtonlgusmao <wellingtonluizgusmao.29@gmail.com>
2022-10-18 15:21:07 -0300

    mod05

---
+++ metodos-java/src/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+            
+            System.out.println("tv ligada" + smartTv.ligada);
+            System.out.println("canal atual" + smartTv.canal);
+            System.out.println("volume atual" + smartTv.volume);
+
+
+
+}
+}
+
+
